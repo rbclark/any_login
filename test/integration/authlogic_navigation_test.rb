0c51b2252cf76ecbc8bd7ad349f6f3d3e9609669
Robert Clark <robert.clark@kirkpatrickprice.com>
2022-08-25 16:49:27 -0400

    Reproduce error when trying to load multiple applications

---
+++ test/integration/authlogic_navigation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper_authlogic'
+
+class AuthlogicNavigationTest < ActionDispatch::IntegrationTest
+  test "navigate to the homepage" do
+
+  end
+end
---
+++ test/integration/devise_navigation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper_devise'
+
+class DeviseNavigationTest < ActionDispatch::IntegrationTest
+  test "navigate to the homepage" do
+
+  end
+end
